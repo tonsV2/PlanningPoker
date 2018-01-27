6bb0e7edbdd3afaaaa8599f985396ccb2aea9feb
ahj <ahj@altapay.com>
2018-01-27 18:10:45 -0200

    Initial commit

---
+++ backend/src/main/kotlin/dk/fitfit/planning/backend/PlanningPokerBackendApplication.kt
@@ -0,0 +1,11 @@
+package dk.fitfit.planning.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanningPokerBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlanningPokerBackendApplication>(*args)
+}
